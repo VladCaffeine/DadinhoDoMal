b7044a8330000623e236c99ac1c185ebac4bdad2
VladCaffeine <vladimirprodugbi15@gmail.com>
2023-08-10 19:44:18 -0300

    Add project files.

---
+++ DaciGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program 
+{
+    public static void Main(string[] args) 
+    {
+        Random random = new Random();
+
+        int PlayerOne;
+        for (int i = 0; i < 10; i++) 
+        {
+            Console.Write("Prees a key to choose your dice: ");
+
+            Console.ReadKey(); // waiting for a keyboard command
+
+            PlayerOne = random.Next(1, 7);
+            Console.WriteLine("Your number is" + PlayerOne);
+        }
+
+    }
+}
